90eb97c174879a69eefa5330093b1dbbf944045e
Toby Bessant <toby.bessant.1998@gmail.com>
2020-01-12 18:32:49 +0000

    Altered Vue boilerplate

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>TOBY BESSANT</h1>
+		<p>
+			A motivated final year student studying Computer Science & Games
+			Development at Plymouth University, graduating in June 2020.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<style scoped>
+
+</style>
